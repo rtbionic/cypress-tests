cc33a789a2fb24e099584bf0cc4508bf4f6fe84b
rtbionic <r.team.bionic@gmail.com>
2025-03-27 17:51:07 +0300

    added get, contains, find, eq, choldren tests for homework

--- cypress/e2e/homework_1.cy.js
+++ cypress/e2e/homework_1.cy.js
@@ -3,7 +3,27 @@ describe('template spec', () => {
     cy.visit('https://by-portal-shopping-cart-new.spnode.net/external/')
   })
 
-  it('test 1', () => {
+  it('get', () => {
     cy.get('[data-automation-id="step_title"]')
+  })
+
+  it('contains', () => {
+    cy.get('[data-automation-id="subscription_p_name"]').contains('Mobile')
+  })
+
+  it('find', () => {
+    cy.get('[data-automation-id="subscription_plans"]').find(
+      '[data-automation-id="subscription_p"]'
+    )
+  })
+
+  it('eq', () => {
+    cy.get('[data-automation-id="subscription_plans"]')
+      .find('[data-automation-id="subscription_p"]')
+      .eq(1)
+  })
+
+  it('children', () => {
+    cy.get('[data-automation-id="subscription_plans"]').children()
   })
 })
