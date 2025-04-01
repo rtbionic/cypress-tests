f4aa00df3a0c1ca17587df9e64c399e4154646e6
rtbionic <r.team.bionic@gmail.com>
2025-04-01 18:15:34 +0300

    fixed homework

--- cypress/e2e/homework_1.1.cy.js
+++ cypress/e2e/homework_1.1.cy.js
@@ -12,12 +12,12 @@ describe('template spec', () => {
         .children('[data-automation-id="subscription_p_select_btn"]')
         .click()
         //if language != 'English'
-        //cy.get('[data-automation-id="select_lang"]').click()
-        //cy.get('[data-automation-id="lang"]').children('button').contains('English').parent('span').parent('button').click()
+        cy.get('[data-automation-id="select_lang"]').click()
+        cy.get('[data-automation-id="lang"]').children('button').contains('English').parent('span').parent('button').click()
         cy.get('[data-automation-id="step_title"]')
     })
 
-    it('1.click', () => {
+    it('2.click-type', () => {
         cy.get('[data-automation-id="subscription_plans"]')
         .find('[data-automation-id="subscription_p"]')
         .eq(0)
@@ -26,9 +26,10 @@ describe('template spec', () => {
         .children('[data-automation-id="subscription_p_select_btn"]')
         .click()
         //if language != 'English'
-        //cy.get('[data-automation-id="select_lang"]').click()
-        //cy.get('[data-automation-id="lang"]').children('button').contains('English').parent('span').parent('button').click()
-        cy.get('[data-automation-id="step_title"]')
+        cy.get('[data-automation-id="select_lang"]').click()
+        cy.get('[data-automation-id="lang"]').children('button').contains('English').parent('span').parent('button').click()
+        cy.get('[data-automation-id="order_title"]').next('div').next('div').children('[data-automation-id="order_continue_btn"]').click()
+        cy.get('[data-automation-id="userName"]').children('div').children('div').children('[name="userName"]').click().type('artem')
     })
 
   })
