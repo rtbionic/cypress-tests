8024803d3579715c607e49c601a7cc79b2d357ff
rtbionic <r.team.bionic@gmail.com>
2025-04-03 18:18:38 +0300

    added 3.hard_level to haomework_1.2

--- cypress/e2e/homework_1.2.cy.js
+++ cypress/e2e/homework_1.2.cy.js
@@ -70,4 +70,48 @@ describe('template spec', () => {
         .and('exist')
     })
   })
+
+  it.only('3.hard_level', () => {
+    cy.get('[data-automation-id="select_lang"]').then(($el) => {
+      if ($el.not('span', 'contains("EN")')) {
+        cy.get('[data-automation-id="select_lang"]').click()
+        cy.get('[data-automation-id="lang"]')
+          .children('button')
+          .contains('English')
+          .parent('span')
+          .parent('button')
+          .click()
+      } else {
+        return
+      }
+    })
+    cy.get('[data-automation-id="subscription_plans"]')
+      .find('[data-automation-id="subscription_p"]')
+      .eq(0)
+      .children('div')
+      .children('div')
+      .children('[data-automation-id="subscription_p_select_btn"]')
+      .click()
+    //enter login information
+    cy.get('[data-automation-id="order_continue_btn"]').click()
+    cy.get('[name="userName"]').click().type('artem')
+    //.invoke('text')
+    //.then(($text) => {
+    //cy.get('[name="userName"]')
+    //cy.then(() => {
+    //expect($text).to.eq('artem')
+    //})
+    //})
+    cy.get('[name="email"]').click().type('art.titovich@gmail.com')
+    cy.get('[name="password"]').click().type('159753')
+    cy.get('[name="confirmPassword"]').click().type('159753')
+    cy.get('[name="firstName"]').click().type('Artem')
+    cy.get('[name="lastName"]').click().type('Titovich')
+    cy.get('[name="phone"]').click().type('375298104421')
+    cy.get('[name="address"]').click().type('Nekrasova 35')
+    cy.get('[aria-haspopup="listbox"]').click()
+    cy.get('[role="listbox"]').children('[value="BY"]').click()
+    cy.get('[name="city"]').click().type('Minsk')
+    cy.get('[name="zipCode"]').click().type('220040')
+  })
 })
